a8d50df1d6f9933dc41ab0b2e4b5d2ee567e0e70
Kou-Ro <koushi.rockv@gmail.com>
2022-10-09 20:23:19 +0900

    Handover

---
+++ CustomOutput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoderAutomationTool
+{
+    internal class CustomOutput
+    {
+        internal static void ColorWriteLine(string s,ConsoleColor c)
+        {
+            Console.ForegroundColor = c;
+            Console.WriteLine(s);
+            Console.ResetColor();
+        }
+
+        internal static void ColorWrite(string s,ConsoleColor c)
+        {
+            Console.ForegroundColor = c;
+            Console.Write(s);
+            Console.ResetColor();
+        }
+    }
+}
