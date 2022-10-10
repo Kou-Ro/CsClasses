d2d1ac2b008a06e11d95960264d18c27916d1d31
Kou-Ro <koushi.rockv@gmail.com>
2022-10-11 03:11:11 +0900

    Add a function to testrun

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CsClasses
+{
+    public class Control
+    {
+        public static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write("class :");
+                string classname=Console.ReadLine();
+                if (classname=="CustomOutput")RunCustomOutput();
+                else if (classname=="quit")break;
+            }
+        }
+
+        private static void RunCustomOutput()
+        {
+            Console.Write("text :");
+            string text=Console.ReadLine();
+            Console.Write("Color (green or red(default green)) :");
+            string color=Console.ReadLine();
+            Console.Write("Write mode (WriteLine or Write(default WriteLine)) :");
+            string mode=Console.ReadLine();
+
+            if(mode=="Write")CustomOutput.ColorWriteLine(text,color=="red"?ConsoleColor.Red:ConsoleColor.Green);
+            else CustomOutput.ColorWrite(text,color=="green"?ConsoleColor.Red:ConsoleColor.Green);
+        }
+    }
+}
